978f1bb97e0fd69cb77eeb7c503ce3a64f40d652
doDomoGu <kaka_71936410@qq.com>
2016-07-31 11:04:20 +0800

    xinmengweb 这个主题里 去除 其他主题信息

--- wp-content/themes/songtang.net/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    get_header();
-?>
-
-<?php echo 'index';?>
--- wp-content/themes/songtang.net/sider_right.php
+++
@@ -1,9 +0,0 @@
-<a target="_blank">
-    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/common/intro_right_1.jpg">
-</a>
-<a target="_blank">
-    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/common/intro_right_2.jpg">
-</a>
-<a target="_blank">
-    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/common/intro_right_3.jpg">
-</a>
